023463d028de44f6df5c7bed9e771ef0b6957b56
toonvanderlely <toon.vanderlely@gmail.com>
2021-10-12 13:03:05 +0200

    info toegevoegd aan home pagina

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>Codecamp</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+<?php require_once 'header.php'; ?>
+<div class="container">
+    <h1>Info</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla dolorum enim quia cumque animi, possimus earum qui a fugiat dolorem officia sed inventore laudantium, recusandae quas iusto dolore vitae.</p>
+</div>
+    
+    
+</body>
+<footer>
+    <?php require_once 'footer.php'; ?>
+</footer>
+</html>
 
