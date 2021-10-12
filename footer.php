772b9c7277c0c5803524d9dbea5bb3ef57f8c9f8
toonvanderlely <toon.vanderlely@gmail.com>
2021-10-12 14:34:20 +0200

    footer update html

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <!-- Site footer -->
 <footer class="site-footer">
-      </div>
       <div class="container">
         <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
+          <div class="text">
             <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
          <a href="#">Codecamp</a>.
             </p>
