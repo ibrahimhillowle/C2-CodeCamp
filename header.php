25006ea3e74095d5b74cbe237737d14b8c40d8e8
toonvanderlely <toon.vanderlely@gmail.com>
2021-10-14 14:04:23 +0200

    tijd toegevoegd aan nav

--- header.php
+++ header.php
@@ -11,6 +11,10 @@
   <div class="subnav-logo">
     <a href="index.php">codecamp</a>
   </div>
+  <div class="subnav-time">
+    <a href="https://24timezones.com" style="text-decoration: none" class="clock24" id="tz24-1634212533-c116-eyJob3VydHlwZSI6IjI0Iiwic2hvd2RhdGUiOiIwIiwic2hvd3NlY29uZHMiOiIwIiwic2hvd3RpbWV6b25lIjoiMCIsInR5cGUiOiJkIiwibGFuZyI6ImVuIn0=" title="time now" target="_blank" rel="nofollow"></a>
+    <script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
+  </div>
   <div class="subnav">
     <button class="subnavbtn">Informatie <i class="fa fa-caret-down"></i></button>
     <div class="subnav-content">
