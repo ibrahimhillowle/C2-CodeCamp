3947baafe84f397ac1b6c13475e49b3541d7c1aa
toonvanderlely <toon.vanderlely@gmail.com>
2021-10-14 14:40:10 +0200

    Create jamesbond.php

---
+++ jamesbond.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Codecamp</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+<?php require_once 'header.php'; ?>
+<div class="container">
+    <h1>Lijst met alle jamesbond films:</h1>
+    <br>
+    <ul>
+        <li>Dr.no</li>
+        <li>From russia with love</li>
+        <li>Goldfinger</li>
+        <li>Thunderball</li>
+        <li>You only live twice</li>
+        <li>On Her Majesty's Secret Service</li>
+        <li>Diamonds Are Forever</li>
+        <li>Live and Let Die</li>
+        <li>The Man with the Golden Gun</li>
+        <li>The Spy Who Loved Me</li>
+        <li>Moonraker</li>
+        <li>For Your Eyes Only</li>
+        <li>Octopussy</li>
+        <li>A View to a Kill</li>
+        <li>The Living Daylights</li>
+        <li>Licence to Kill</li>
+        <li>GoldenEye</li>
+        <li>Tomorrow Never Dies</li>
+        <li>The World Is Not Enough</li>
+        <li>Die Another Day</li>
+        <li>Casino Royale</li>
+        <li>Quantum of Solace</li>
+        <li>Skyfall</li>
+        <li>Spectre</li>
+        <li>No Time to Die</li>
+    </ul>
+</div>
+
+
+</body>
+<footer>
+    <?php require_once 'footer.php'; ?>
+</footer>
+</html>
