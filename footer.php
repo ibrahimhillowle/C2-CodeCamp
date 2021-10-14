4e56853f782e83d1956fe04fb4a2ffb0178d0a9e
toonvanderlely <toon.vanderlely@gmail.com>
2021-10-14 14:13:15 +0200

    contact informatie toegevoegd aan footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <div class="row">
           <div class="text">
             <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
-         <a href="#">Codecamp</a>.
+         <a href="#">Codecamp</a>, <a href = "mailto: codecamp@gmail.com">Contact us!</a>
             </p>
           </div>
         </div>
