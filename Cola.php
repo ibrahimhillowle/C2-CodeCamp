51d98e7771b1298de9d82ea201c9a9e734555aec
guy van wonderen <guy.yves.vanwonderen@gmail.com>
2021-10-15 14:54:32 +0200

    cola pagina
    cola pagina aan gemaakt

---
+++ Cola.php
@@ -0,0 +1,38 @@
+   <title>Coca-Cola</title>
+</head>
+<body class="orange">
+<?php require_once 'header.php'; ?>   
+
+
+<div class="container">
+
+    <h1>Coca Cola</h1>
+    <h2 STYLE="font-style: italic; color: grey;">Kan ook warm gedronken worden ~ Toon</h2>
+<div class="song">
+
+    <p class ="lyrics">
+        Coca-Cola is een merk cola. 
+        Het is een suikerhoudende koolzuurhoudende frisdrank die wordt verhandeld door The Coca-Cola Company. 
+        Het is sinds 1893 als handelsmerk geregistreerd. Inmiddels zijn het logo,
+        de flessen en de drank samen een wereldwijd bekend icoon voor de Amerikaanse levensstijl.  
+
+        Coca-Cola is bedacht in 1886 door de apotheker John Pemberton die eerder een variant
+        op de destijds populaire alcoholische Vin Mariani ontwikkelde die hij Pemberton's French wine coca noemde.
+        Nadat in Pembertons thuisbasis Atlanta in 1886 een verbod op alcoholische dranken was uitgevaardigd,
+        verving hij de wijn door een extract van kolanoten. 
+        Om de bittere smaak van de kolanoten te neutraliseren voegde hij er suikersiroop aan toe en noemde het drankje Coca-Cola. 
+        Het werd verkocht in kruikjes en moest worden aangelengd met water. 
+        Toen men de siroop ging mengen met koolzuurhoudend water ontstond de prikkelende drank met de kenmerkende colasmaak
+    <img class="FrissdrankImg" src="img/cola.png" alt="CocaCola">
+    </p>
+   
+
+
+</div>
+</div>
+
+<footer>
+    <?php require_once 'footer.php'; ?>
+</footer>
+</body>
+</html>
