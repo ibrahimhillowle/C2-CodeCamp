14c179dfb2a420321558674490ad0eb2b7264066
toonvanderlely <toon.vanderlely@gmail.com>
2021-10-15 14:43:05 +0200

    lukeskywalker

--- footer.php
+++ footer.php
@@ -3,8 +3,14 @@
       <div class="container">
         <div class="row">
           <div class="text">
+            <h1>Contact informatie</h1>
+            <ul>
+              <li>Mail: Codecamp@gmail.com</li>
+              <li>Phone: 0611228837</li>
+              <li>Adress: Terheijdenseweg 350, 4826 AA Breda</li>
+            </ul>
             <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
-         <a href="#">Codecamp</a>, <a href = "mailto: codecamp@gmail.com">Contact us!</a>
+              <a href="#">Codecamp</a>
             </p>
           </div>
         </div>
