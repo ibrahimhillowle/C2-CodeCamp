26c4a7842e51bde22d2a3dd3c4d11a97014114f8
Ibrahim <72141548+ibrahimhillowle@users.noreply.github.com>
2021-10-15 14:13:24 +0200

    fix #23
    schroevendraaiers ppagina gemaakt

---
+++ schroevendraaiers.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Codecamp</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+<?php require_once 'header.php'; ?>
+<div class="container">
+    <h1>schroevendraaiers</h1>
+    <br>
+    <p>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
+Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers met een platte punt, alsook met een ster- of kruiskop. Goede schroevendraaiers zijn vervaardigd van speciale staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'.
+
+</p>
+</div>
+
+
+</body>
+<footer>
+    <?php require_once 'footer.php'; ?>
+</footer>
+</html>
