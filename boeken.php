64c54d9e5b867daed72da23920943b6952db7625
guy van wonderen <guy.yves.vanwonderen@gmail.com>
2021-11-01 11:03:01 +0100

    Update boeken.php

--- boeken.php
+++ boeken.php
@@ -13,8 +13,49 @@
     <p>Het boek is een veelgebruikte manier om informatie vast te leggen in de vorm van tekst en afbeeldingen en om deze informatie weer te geven, op te slaan en te verspreiden. Het is een document van vaak niet al te kleine omvang. Inhoud Omschrijving en etymologie Een boek in traditionele zin is een samengevoegde hoeveelheid papier met tekst, afbeeldingen of allebei. Daarnaast wordt met de term de betreffende informatie, of een soortgelijke verzameling informatie, aangeduid ('een boek schrijven', 'een boek downloaden'). Het woord 'boek' is vermoedelijk afkomstig van het Germaanse woord voor beuk, boche. De omslagen van de eerste boeken werden namelijk gemaakt van een rechthoekig stuk (beuken)hout.</p> 
     <img src="img/boek.jpg" alt="Foto van een boek" width="970px">
 </div>
+<div class="boekenlijst">
+     <h1>FrisdrankHarry Potter en de steen de Wijzen – JK Rowling</h1>
+    <div class="WarmeDranken">
+        <div class="WarmeDranken-1">
+            <h1>
+             Cola
+            </h1>
+            <div class="img-container">
+            <img src="img/cola.png" alt="Cola">
+            </div>
+            <p>
+                De eerste cola was Coca-Cola, een alcoholvrije imitatie door John Pemberton, 
+                van de Vin Mariani die door Angelo Mariani was bedacht.
+            </p>
+        </div>
+        <div class="WarmeDranken-2">
+            <h1>
+              7-Up
+            </h1>
+            <div class="img-container">
+            <img src="img/7Up.png" alt="7-UP">
+            </div>
+            <p>T
+                Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group
+                in de Verenigde Staten en PepsiCo in de rest van de wereld.
+            </p>
+        </div>
+        <div class="WarmeDranken-3">
+            <h1>
+             Sinas
+            </h1>
+            <div class="img-container">
+            <img src="img/sinas.png" alt="Latte">
+            </div>
+            <p>
+                Sinas is in Nederland de benaming voor de doorgaans gele of oranje,
+                meestal koolzuurhoudende frisdrank met sinaasappelsmaak.
+            </p>
+          </div>
+        </div>
 
-
+    </div>
+</div>
 </body>
 <footer>
     <?php require_once 'footer.php'; ?>
