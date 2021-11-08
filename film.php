c003456b964a0a1352486f91a5ae17f828cd833f
Ibrahim <72141548+ibrahimhillowle@users.noreply.github.com>
2021-11-08 08:53:54 +0100

    film

--- film.php
+++ film.php
@@ -8,10 +8,13 @@
 <body>
 <?php require_once 'header.php'; ?>
 <div class="container">
-    
-    
-</div>
+    <div class="acteurs">
+        <h2>Acteurs</h2>
+        <p>Een acteur kan zijn werk doen in een theatervoorstelling (theateracteur) of film (filmacteur), op televisie, tijdens communicatietrainingen als trainingsacteur en hij kan als zijn stem lenen aan tekenfilmfiguren en reclameboodschappen (stemacteur). Acteurs kunnen ook een personage in een computerspel uitbeelden, of als simulatiemodel hiervoor worden ingezet waarna deze gerenderd worden. Elk van deze disciplines vereist een eigen techniek.
 
+Een acteur kan lijken op zijn personage, maar kan er ook in allerlei opzichten van verschillen - zelfs in leeftijd of sekse. Een acteur kan voor zijn rol sommige lichamelijke aspecten moeten aannemen, zoals bijvoorbeeld kapsel, gezichtsbeharing of lichaamsomvang. Met schmink of siliconenmasker kan een andere gelaatsuitdrukking of zelfs een ander gelaat worden aangenomen, met een pruik kan de haardracht worden aangepast.</p>
+
+   
 
 </body>
 <footer>
--- header.php
+++ header.php
@@ -39,20 +39,8 @@
     <div class="dropdown-content">
         <a href="film.php">Film</a>
         <a href="boeken.php">Boeken</a>
-        <a href="jamesbond.php">jamesbond</a>
-        <a href="#">Link 3</a>
     </div>
-  </div>
-   <div class="dropdown">
-    <button class="dropbtn">Overige 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-     
-      <a href="video.php">Film links</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div>
+  </div>              
   <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
   <div class="cleanslate w24tz-current-time w24tz-small" style="display: inline-block !important; visibility: hidden !important; min-width:200px !important; min-height:100px !important;"><p><a href="//24timezones.com/Netherlands/time" style="text-decoration: none" class="clock24" id="tz24-1635858906-cc14361-eyJob3VydHlwZSI6IjI0Iiwic2hvd2RhdGUiOiIxIiwic2hvd3NlY29uZHMiOiIwIiwiY29udGFpbmVyX2lkIjoiY2xvY2tfYmxvY2tfY2I2MTgxMzlkYTUwMDFmIiwidHlwZSI6ImRiIiwibGFuZyI6ImVuIn0=" title="Netherlands time" target="_blank" rel="nofollow">Current time in Netherlands</a></p><div id="clock_block_cb618139da5001f"></div></div>
 <script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
