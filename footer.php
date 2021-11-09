ad218f4849c881e73d661cc68e62b573605f7d83
aliashunter <elia1231@hotmail.com>
2021-11-09 12:43:10 +0100

    Add files via upload
    Plattegrond toegevoegd

--- footer.php
+++ footer.php
@@ -3,15 +3,18 @@
       <div class="container">
         <div class="row">
           <div class="text">
+            <div class="plattegrond">
             <h1>Contact informatie</h1>
             <ul>
               <li>Mail: Codecamp@gmail.com</li>
               <li>Phone: 0611228837</li>
-              <li>Adress: Terheijdenseweg 350, 4826 AA Breda</li>
+              <li>Adress: Koestraat 1, 5223 AG 's-Hertogenbosch</li>
             </ul>
-            <p class="copyright-text">Copierecht &copy; 2021 Alle rechten voorbehouden door
+            <img src="img/Koestraat1.png" alt="plattegrond">
+            <p class="copyright-text">Copyright &copy; 2021 Alle rechten voorbehouden door
               <a href="#">Codecamp</a>
             </p>
+          </div>
           </div>
         </div>
       </div>
