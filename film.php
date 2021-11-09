cffe2bf28c0a588a6d87aa71cea2e719547fdac3
Ibrahim <72141548+ibrahimhillowle@users.noreply.github.com>
2021-11-09 12:23:54 +0100

    flim pagina aangepast

--- film.php
+++ film.php
@@ -9,12 +9,15 @@
 <body>
   <?php require_once 'header.php'; ?>
   <div class="container">
-  <div class="film-container">
-  <h2>Acteurs</h2>
+
+
+    <div class="over-acteurs">
+      <h2>Acteurs</h2>
       <p>Een acteur kan zijn werk doen in een theatervoorstelling (theateracteur) of film (filmacteur), op televisie,
         tijdens communicatietrainingen als trainingsacteur en hij kan als zijn stem lenen aan tekenfilmfiguren en
         reclameboodschappen (stemacteur). Acteurs kunnen ook een personage in een computerspel uitbeelden, of als
-        simulatiemodel hiervoor worden ingezet waarna deze gerenderd worden. Elk van deze disciplines vereist een eigen
+        simulatiemodel hiervoor worden ingezet waarna deze gerenderd worden. Elk van deze disciplines vereist een
+        eigen
         techniek.
 
         Een acteur kan lijken op zijn personage, maar kan er ook in allerlei opzichten van verschillen - zelfs in
@@ -22,28 +25,48 @@
         bijvoorbeeld kapsel, gezichtsbeharing of lichaamsomvang. Met schmink of siliconenmasker kan een andere
         gelaatsuitdrukking of zelfs een ander gelaat worden aangenomen, met een pruik kan de haardracht worden
         aangepast.</p>
-    <div class="acteurs">
-    <div class="jamesbond">
-        <h2><a href="rogermoore.php">Roger Moore</a></h2>
-        
-      </div>
-      <div class="georgeclooney">
-        <h2><a href="georgeclooney.php">georgeclooney</a></h2>
-      </div>
-    </div>
-    <div class="characters">
-       <div class="lukeskywalker">
-           <h2><a href="lukeskywalker.php">lukeskywalker</a></h2>
-       </div>
 
     </div>
-    <div class="darthvader">
-        <a href="darthvader.php"><h2>darthvader</h2></a>
+
+    <div class="acteurs">
+
+
+      <h2><a href="rogermoore.php">Roger Moore</a></h2>
+
+      <h2><a href="georgeclooney.php">georgeclooney</a></h2>
+
     </div>
-    <div class="timothydalton phptimothydalton">
-        <a href="timothydalton.php"><h2>timothydalton</h2></a>
+    <div class="over-personage">
+      <h2>personage</h2>
+      <p>Een personage is de uitbeelding van een persoon in een verhalend of dramatisch kunstwerk zoals een roman,
+        toneelstuk of film. Personages ondergaan of veroorzaken de gebeurtenissen in een verhaal.
+
+        In films en toneelstukken is het de rol die door een acteur of actrice gespeeld wordt. In rollenspellen is het
+        de rol die een speler aanneemt binnen de wereld van het spel. In rollenspellen en computerspellen wordt
+        hiervoor in het Nederlands ook wel het anglicisme karakter (Engels: character) gebruikt.
+
+        Het woord personage stamt af van het Latijnse woord persona, dat "masker" betekent. In de klassieke oudheid
+        droegen toneelacteurs namelijk altijd maskers. Een personage in een beeldverhaal wordt ook wel een stripfiguur
+        genoemd. </p>
+
+
     </div>
-  </div>
+
+    <div class="personage">
+
+      <h2><a href="lukeskywalker.php">lukeskywalker</a></h2>
+
+
+      <h2><a href="darthvader.php">darthvader</a></h2>
+
+
+      <h2><a href="timothydalton.php">timothydalton</a></h2>
+    </div>
+
+
+
+
+
   </div>
 </body>
 <footer>
