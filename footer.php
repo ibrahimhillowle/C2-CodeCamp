13cdf869daec8ceea20394689692577c5d7b84e9
toonvanderlely <toon.vanderlely@gmail.com>
2021-11-11 14:23:10 +0100

    map toegevoegd

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
               <li>Phone: 0611228837</li>
               <li>Adress: Koestraat 1, 5223 AG 's-Hertogenbosch</li>
             </ul>
-            <img src="img/Koestraat1.png" alt="plattegrond">
+            <div class="mapouter"><div class="gmap_canvas"><iframe width="319" height="297" id="gmap_canvas" src="https://maps.google.com/maps?q=Koestraat%201,%205223%20AG%20's-Hertogenbosch&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.whatismyip-address.com/divi-discount/"></a><br><style>.mapouter{position:relative;text-align:right;height:297px;width:319px;}</style><a href="https://www.embedgooglemap.net">google maps on my website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:297px;width:319px;}</style></div></div>
             <p class="copyright-text">Copyright &copy; 2021 Alle rechten voorbehouden door
               <a href="#">Codecamp</a>
             </p>
